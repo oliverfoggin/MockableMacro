df62206e609cd13248b3d78f1769587b82f07c40
Oliver Foggin <oliverfoggin@me.com>
2024-09-01 20:09:33 +0100

    Swift Syntax version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "508.0.1"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", .upToNextMajor(from: "0.5.2")),
     ],
     targets: [
